a852300daef1423f573ffecaf0f90e73f76ee059
kkt-623 <s163404@gmail.com>
2022-06-17 00:00:14 +0900

    コンポーネント化1 index.jsの記述をコピー

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>　Knock Knock</h1>
+      <p>こんにちは</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
