c12627a352e9246194004070252a918a719d4519
kkt-623 <s163404@gmail.com>
2022-06-16 23:50:19 +0900

    要素をさらに追加（SyntaxErrorが出る） 以後のコミットでエラー修正する

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <h1>　Knock Knock</h1>;
+  return (
+    <h1>　Knock Knock</h1>
+    <p>こんにちは</p>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
