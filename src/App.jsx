c173686f7caf870a0bf80ea1aaff4b298a345c4a
kkt-623 <s163404@gmail.com>
2022-06-17 00:26:50 +0900

    基本のイベント定義 - JSX記法では{}で囲むとJavascriptを記述できる

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 export const App = () => {
+  // ボタンを押したときの処理
+  const onClickButton = () => {
+    alert();
+  };
+
   return (
     <div>
       <h1>　Knock Knock</h1>
       <p>こんにちは</p>
+      <button onClick={onClickButton}>ボタン</button>
     </div>
   );
 };
