6f82fe3a4b0b0ea818a4153bddb2b053094957c3
kkt-623 <s163404@gmail.com>
2022-07-11 15:16:39 +0900

    Propsを引数の段階で展開

--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,6 +1,6 @@
-export const ColoredMessage = (props) => {
+export const ColoredMessage = ({ color, children }) => {
   // Propsを分割代入
-  const { color, children } = props;
+  //const { color, children } = props;
   const contentStyle = {
     color,
     fontSize: "20px"
