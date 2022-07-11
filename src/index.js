10f4eebee0363485ce1004fe8b67a1210cee391d
kkt-623 <s163404@gmail.com>
2022-07-11 16:49:00 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import { CssModules } from "./CssModules";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<CssModules />, document.getElementById("root"));
